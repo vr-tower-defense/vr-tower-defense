84ee35d1fca4e11d2d0d61ca5418f11b4fdc5bf7
Lex van der Stap <lex_olifant@hotmail.com>
2019-04-12 12:03:07 +0200

    Removed object from scene, name changes

--- src/Assets/LightSkyDefense/Scripts/BulletShootScript.cs
+++ src/Assets/LightSkyDefense/Scripts/BulletShootScript.cs
@@ -4,6 +4,7 @@ namespace Assets
 {
     public class BulletShootScript : MonoBehaviour
     {
+        public Rigidbody Projectile;
         public Transform BulletSpawn;
 
         // Use this for initialization
@@ -17,6 +18,7 @@ namespace Assets
         {
             if (Input.GetButtonDown("Fire1"))
             {
+                var clone = (Rigidbody)Instantiate(Projectile, BulletSpawn.position, Projectile.rotation);
 
                 clone.velocity = BulletSpawn.TransformDirection(Vector3.forward * 20);
             }
