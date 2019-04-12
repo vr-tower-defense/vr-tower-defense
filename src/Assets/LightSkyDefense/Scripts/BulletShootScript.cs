62b6a423e5c8477bacd82c463572b90236fb430f
Lex van der Stap <lex_olifant@hotmail.com>
2019-04-12 12:01:19 +0200

    Added base tower, base bullet, tower's ability to shoot, bullet's ability to delete itself

---
+++ src/Assets/LightSkyDefense/Scripts/BulletShootScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Assets
+{
+    public class BulletShootScript : MonoBehaviour
+    {
+        public Transform BulletSpawn;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+
+                clone.velocity = BulletSpawn.TransformDirection(Vector3.forward * 20);
+            }
+        }
+    }
+}
