ef28c8c0a78b4cf42ea4815a740e52d1646be5ce
hassanAlAwwadi <hassan.awwadi@gmail.com>
2019-04-12 12:03:17 +0200

    Destroy the credits when picked up

--- src/Assets/LightSkyDefense/Scripts/Credit.cs
+++ src/Assets/LightSkyDefense/Scripts/Credit.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Dynamic;
 using UnityEngine;
 using Valve.VR.InteractionSystem;
 
@@ -8,10 +9,15 @@ public class Credit : MonoBehaviour
 {
     public const int value = 5;
         
+    /// <summary>
+    /// When the credit touches the hand, the credit gets added to the players total credit count
+    /// </summary>
+    /// <param name="hand"></param>
     private void OnHandHoverBegin(Hand hand)
     {
         var player = Player.instance.gameObject;
         var creditOwner = player.GetComponent<CreditOwner>();
         creditOwner?.AddCredits(value);
+        Destroy(gameObject);
     }
 }
