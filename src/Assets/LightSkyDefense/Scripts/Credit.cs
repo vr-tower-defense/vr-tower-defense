e8ef178b1b7cf52809e799bbe53f52d0fac51922
hassanAlAwwadi <hassan.awwadi@gmail.com>
2019-04-16 13:03:55 +0200

    credit sound

--- src/Assets/LightSkyDefense/Scripts/Credit.cs
+++ src/Assets/LightSkyDefense/Scripts/Credit.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Dynamic;
 using UnityEngine;
@@ -26,8 +27,10 @@ public class Credit : MonoBehaviour
 
         if (creditOwner == null) { return; }
 
+       
         creditOwner.AddCredits(Value);
         source.PlayOneShot(pickupSound);
+        
 
         Destroy(gameObject);
     }
