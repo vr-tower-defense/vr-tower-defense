7f4e9f212e1ce6d07a4df94921c1cf2d104f8587
hassanAlAwwadi <hassan.awwadi@gmail.com>
2019-04-16 15:56:00 +0200

    don't spawn credit on application quit

--- src/Assets/LightSkyDefense/Scripts/SpawnCreditOnDestruction.cs
+++ src/Assets/LightSkyDefense/Scripts/SpawnCreditOnDestruction.cs
@@ -6,6 +6,13 @@ public class SpawnCreditOnDestruction : MonoBehaviour
 {
     public Credit Credit;
     public int Value;
+
+    private bool isQuitting = false;
+    void OnApplicationQuit()
+    {
+        isQuitting = true;
+    }
+
     /// <summary>
     /// Spawn money when its gameObject is destroyed.
     /// </summary>
@@ -13,5 +20,11 @@ public class SpawnCreditOnDestruction : MonoBehaviour
     {
         Credit.Value = Value;
         Instantiate(Credit, gameObject.transform.position, gameObject.transform.rotation);
+        if (!isQuitting)
+        {
+
+            Credit.Value = Value;
+            Instantiate(Credit, gameObject.transform.position, gameObject.transform.rotation);
+        }
     }
 }
