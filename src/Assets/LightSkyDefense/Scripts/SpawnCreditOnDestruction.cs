0c7154d25d9257b04af721dbddfcc5ce5c46da98
hassanAlAwwadi <hassan.awwadi@gmail.com>
2019-04-16 14:54:29 +0200

    don't spawn new object on application quit

--- src/Assets/LightSkyDefense/Scripts/SpawnCreditOnDestruction.cs
+++ src/Assets/LightSkyDefense/Scripts/SpawnCreditOnDestruction.cs
@@ -6,12 +6,23 @@ public class SpawnCreditOnDestruction : MonoBehaviour
 {
     public Credit Credit;
     public int Value;
+
+    private bool isQuitting = false;
+    void OnApplicationQuit()
+    {
+        isQuitting = true;
+    }
+
     /// <summary>
     /// Spawn money when its gameObject is destroyed.
     /// </summary>
     void OnDisable()
     {
-        Credit.Value = Value;
-        Instantiate(Credit, gameObject.transform.position, gameObject.transform.rotation);
+        if (!isQuitting)
+        {
+
+            Credit.Value = Value;
+            Instantiate(Credit, gameObject.transform.position, gameObject.transform.rotation);
+        }
     }
 }
