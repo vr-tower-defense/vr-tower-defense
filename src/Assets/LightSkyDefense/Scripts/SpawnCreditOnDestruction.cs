6bc74ca58de67206fa1c74ba053450fee8931a5a
hassanAlAwwadi <hassan.awwadi@gmail.com>
2019-04-16 11:50:02 +0200

    drop creddit on delete script added

---
+++ src/Assets/LightSkyDefense/Scripts/SpawnCreditOnDestruction.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCreditOnDestruction : MonoBehaviour
+{
+    public Credit Credit;
+    public int Value;
+    /// <summary>
+    /// Spawn money when its gameObject is destroyed.
+    /// </summary>
+    void OnDisable()
+    {
+        Credit.Value = Value;
+        Instantiate(Credit, gameObject.transform.position, gameObject.transform.rotation);
+    }
+}
