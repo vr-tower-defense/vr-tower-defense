f532df71a88d4a9e7b1bd1abf2afd7d8264d4510
Lex van der Stap <lex_olifant@hotmail.com>
2019-04-16 12:03:20 +0200

    Towers can detect enemies

---
+++ src/Assets/LightSkyDefense/Scripts/TowerAI.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets
+{
+    public class TowerAI : MonoBehaviour
+    {
+        public Rigidbody Projectile;
+        public Transform ProjectileSpawn;
+        public Transform Target = null;
+        private IEnumerator _coroutine;
+        public float ReloadTime;
+
+        void OnTriggerEnter(Collider target)
+        {
+            Enemy enemyScript = target.gameObject.GetComponent<Enemy>();
+            if (enemyScript == null) return;
+            Target = target.transform;
+        }
+
+        void OnTriggerExit(Collider target)
+        {
+            Enemy enemyScript = target.gameObject.GetComponent<Enemy>();
+            if (enemyScript == null) return;
+            Target = null;
+        }
+
+
+        // Use this for initialization
+        void Start()
+        {
+            _coroutine = Reload(ReloadTime);
+            StartCoroutine(_coroutine);
+        }
+
+        IEnumerator Reload(float waitTime)
+        {
+            while (true)
+            {
+                AimAndShoot();
+                yield return new WaitForSeconds(waitTime);
+            }
+        }
+
+        void AimAndShoot()
+        {
+            if (Target == null) return;
+            transform.LookAt(Target);
+            var newProjectile = (Rigidbody)Instantiate(Projectile, ProjectileSpawn.position, Projectile.rotation);
+            newProjectile.velocity = ProjectileSpawn.TransformDirection(Vector3.forward * 20);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+    }
+}
