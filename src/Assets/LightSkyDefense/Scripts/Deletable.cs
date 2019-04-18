c4f9e8cfdcca571fe44a8c41d197139e67dbd894
hassanAlAwwadi <hassan.awwadi@gmail.com>
2019-04-18 10:39:02 +0200

    Sound effect, deletion on trigger hold.

--- src/Assets/LightSkyDefense/Scripts/Deletable.cs
+++ src/Assets/LightSkyDefense/Scripts/Deletable.cs
@@ -10,8 +10,11 @@ using Valve.VR.InteractionSystem;
 [RequireComponent(typeof(Interactable))]
 public class Deletable : MonoBehaviour
 {
-    public SteamVR_Action_Boolean TriggerClickAction = SteamVR_Input.GetBooleanAction("TriggerClick");
+    private bool _eventSet = false;
+    public AudioClip DestroySound;
 
+    private static GameObject _currentHover;
+    public SteamVR_Action_Boolean TriggerClickAction = SteamVR_Input.GetBooleanAction("TriggerClick");
     /// <summary>
     /// Valdiate properties
     /// </summary>
@@ -21,15 +24,48 @@ public class Deletable : MonoBehaviour
         if (TriggerClickAction == null)
             Debug.LogError("`TriggerClick` action has not been set on this component.");
 
-        if (TriggerClickAction != null)
-            TriggerClickAction.onStateDown += (action, source) => DeleteOnHover();
-
+        //used to check if we need to add to the event
+        else if (!_eventSet)
+        {
+            TriggerClickAction.onStateDown += delegate { DestroyCurrentHover(); };
+            _eventSet = true;
+        }
     }
 
-    private void DeleteOnHover()
+    private void DestroyCurrentHover()
     {
-        if (!gameObject.GetComponent<Interactable>().isHovering) return;
-        
-        Destroy(gameObject);
+        if (_currentHover != null)
+        {
+            AudioSource.PlayClipAtPoint(DestroySound, this.gameObject.transform.position);
+            Destroy(_currentHover);
+        }
     }
+
+    /// <summary>
+    /// If Hovering begins, and the TriggerClickAction State is true, the tower gets destroyed
+    /// Otherwise it becomes the _currentHover object
+    /// </summary>
+    /// <param name="hand"></param>
+    private void OnHandHoverBegin(Hand hand)
+    {
+        if (TriggerClickAction.state)
+        {
+            AudioSource.PlayClipAtPoint(DestroySound, this.gameObject.transform.position);
+            Destroy(this.gameObject);
+        }
+        else
+            _currentHover = this.gameObject;
+        
+    }
+
+    /// <summary>
+    /// IF the _currentHover is the gameObject, remove it.
+    /// </summary>
+    /// <param name="hand"></param>
+    private void OnHandHoverEnd(Hand hand)
+    {
+        if (_currentHover == this.gameObject)
+            _currentHover = null;
+    }
+
 }
