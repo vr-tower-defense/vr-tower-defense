03dcc07f04fe1661e1afccfae0fe5f319def8b75
hassanAlAwwadi <hassan.awwadi@gmail.com>
2019-05-07 10:09:23 +0200

    added on game end event

---
+++ src/Assets/LightSkyDefense/Scripts/OnGameEndTarget.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.EventSystems;
+
+
+interface IOnGameEndTarget : IEventSystemHandler
+{
+    void OnGameWin();
+    void OnGameLoss();
+}
+
