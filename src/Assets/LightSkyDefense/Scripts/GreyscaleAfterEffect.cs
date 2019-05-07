0246758c216500719afbe194e31614141a1cdf4d
hassanAlAwwadi <hassan.awwadi@gmail.com>
2019-05-07 13:05:08 +0200

    code reorder

--- src/Assets/LightSkyDefense/Scripts/GreyscaleAfterEffect.cs
+++ src/Assets/LightSkyDefense/Scripts/GreyscaleAfterEffect.cs
@@ -7,16 +7,17 @@ using UnityEngine;
 
 public class GreyscaleAfterEffect : MonoBehaviour
 {
+    private Material _afterEffectMaterial;
 
     public float Intensity = 1f;
     public bool Active;
-    private Material _afterEffectMaterial;
 
     public void Start()
     {
         _afterEffectMaterial = new Material(Shader.Find("Hidden/Grey"));
         _afterEffectMaterial.SetFloat("_bwBlend", Intensity);
     }
+
     public void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
         if (Active)
