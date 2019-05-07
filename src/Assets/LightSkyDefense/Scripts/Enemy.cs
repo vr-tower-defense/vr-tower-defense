4e88c1113b0d65f5bd9d2f2d0d419ef9bd530f03
Lex van der Stap <lex_olifant@hotmail.com>
2019-05-07 15:06:36 +0200

    #85 Added credit score

--- src/Assets/LightSkyDefense/Scripts/Enemy.cs
+++ src/Assets/LightSkyDefense/Scripts/Enemy.cs
@@ -9,6 +9,7 @@ public class Enemy : MonoBehaviour, IOnGameLossTarget
     public float MovementSpeed = 0.3f;
     public float EnergyCapacity = 40f;
     public float CollisionDamage = 35f;
+    public float PointValue = 10f;
 
     public ParticleSystem ExplodeEffect;
     public ParticleSystem TeleportEffect;
@@ -16,7 +17,7 @@ public class Enemy : MonoBehaviour, IOnGameLossTarget
     public AudioClip TeleportSound;
 
     public Credit Credit;
-    public int CreditValue;
+    public int CreditValue = 5;
 
     private ParticleSystem _explodeEffectInstance = null;
     private ParticleSystem _teleportEffectInstance = null;
@@ -192,6 +193,8 @@ public class Enemy : MonoBehaviour, IOnGameLossTarget
             gameObject.transform.position,
             gameObject.transform.rotation
         );
+
+        GameObject.Find("Scoreboard").GetComponent<Scoreboard>().PointGain(PointValue);
     }
 
     /// <summary>
--- src/Assets/LightSkyDefense/Scripts/Scoreboard.cs
+++ src/Assets/LightSkyDefense/Scripts/Scoreboard.cs
@@ -5,14 +5,15 @@ using Valve.VR;
 
 public class Scoreboard : MonoBehaviour, IOnGameLossTarget
 {
-    public int Score = 0;
+    public float Score;
     public float Timer;
     public float TimeBetweenWaves = 0;
-    public float WaveNumber = 0;
+    public float WaveNumber;
     public float TotalNumberOfWaves = 15;
     public AudioClip BossSpawnedClip;
 
     private float _remaininglives;
+    private float _playercredits;
     private float _timeToNextWave;
     private AudioSource _source;
     private TextMesh _score;
@@ -20,6 +21,7 @@ public class Scoreboard : MonoBehaviour, IOnGameLossTarget
     private TextMesh _timerToNextWave;
     private TextMesh _lives;
     private TextMesh _waveProgression;
+    private TextMesh _credits;
 
 
     // Start is called before the first frame update
@@ -33,12 +35,14 @@ public class Scoreboard : MonoBehaviour, IOnGameLossTarget
         GameObject scoreText = new GameObject();
         _score = scoreText.AddComponent<TextMesh>();
         _score.fontSize = 30;
-        //_score.font = Resources.Load<Font>("Fonts/Potra");
         _score.transform.SetParent(this.transform);
         _score.transform.localScale = new Vector3(0.0005f, 0.0005f, 0.0005f);
         _score.anchor = TextAnchor.MiddleLeft;
+        _score.transform.localPosition = new Vector3(-0.01f, -0.011f, -0.002f);
         //_score.transform.localEulerAngles += new Vector3(0, 0, 0);
-        _score.transform.localPosition = new Vector3(-0.01f, -0.011f, -0.005f);
+        //_score.font = Resources.Load<Font>("Fonts/Potra");
+        Instantiate(scoreText, new Vector3(-0.01f, -0.011f, -0.002f), Quaternion.Euler(0, 0, 180), this.transform);
+
 
         GameObject timerText = new GameObject();
         _timer = timerText.AddComponent<TextMesh>();
@@ -48,7 +52,7 @@ public class Scoreboard : MonoBehaviour, IOnGameLossTarget
         _timer.transform.localScale = new Vector3(0.0005f, 0.0005f, 0.0005f);
         _timer.anchor = TextAnchor.MiddleLeft;
         //_timer.transform.localEulerAngles += new Vector3(90, 0, 0);
-        _timer.transform.localPosition = new Vector3(-0.01f, -0.011f, 0);
+        _timer.transform.localPosition = new Vector3(-0.01f, -0.011f, 0.002f);
 
         GameObject timerToNextWaveText = new GameObject();
         _timerToNextWave = timerToNextWaveText.AddComponent<TextMesh>();
@@ -58,7 +62,7 @@ public class Scoreboard : MonoBehaviour, IOnGameLossTarget
         _timerToNextWave.transform.localScale = new Vector3(0.0005f, 0.0005f, 0.0005f);
         _timerToNextWave.anchor = TextAnchor.MiddleLeft;
         //_timer.transform.localEulerAngles += new Vector3(90, 0, 0);
-        _timerToNextWave.transform.localPosition = new Vector3(-0.01f, -0.011f, 0.005f);
+        _timerToNextWave.transform.localPosition = new Vector3(-0.008f, -0.011f, 0.005f);
 
         GameObject waveProgressionText = new GameObject();
         _waveProgression = waveProgressionText.AddComponent<TextMesh>();
@@ -68,7 +72,7 @@ public class Scoreboard : MonoBehaviour, IOnGameLossTarget
         _waveProgression.transform.localScale = new Vector3(0.0005f, 0.0005f, 0.0005f);
         _waveProgression.anchor = TextAnchor.MiddleLeft;
         //_timer.transform.localEulerAngles += new Vector3(90, 0, 0);
-        _waveProgression.transform.localPosition = new Vector3(0, -0.011f, -0.0025f);
+        _waveProgression.transform.localPosition = new Vector3(-0.004f, -0.011f, -0.005f);
 
         GameObject remainingLivesText = new GameObject();
         _lives = remainingLivesText.AddComponent<TextMesh>();
@@ -76,9 +80,19 @@ public class Scoreboard : MonoBehaviour, IOnGameLossTarget
         //_score.font = Resources.Load<Font>("Fonts/Potra");
         _lives.transform.SetParent(this.transform);
         _lives.transform.localScale = new Vector3(0.0005f, 0.0005f, 0.0005f);
-        _lives.anchor = TextAnchor.MiddleLeft;
+        _lives.anchor = TextAnchor.MiddleRight;
         //_timer.transform.localEulerAngles += new Vector3(90, 0, 0);
-        _lives.transform.localPosition = new Vector3(0, -0.011f, 0.0025f);
+        _lives.transform.localPosition = new Vector3(0.01f, -0.011f, -0.002f);
+
+        GameObject creditsText = new GameObject();
+        _credits = creditsText.AddComponent<TextMesh>();
+        _credits.fontSize = 30;
+        //_score.font = Resources.Load<Font>("Fonts/Potra");
+        _credits.transform.SetParent(this.transform);
+        _credits.transform.localScale = new Vector3(0.0005f, 0.0005f, 0.0005f);
+        _credits.anchor = TextAnchor.MiddleRight;
+        //_timer.transform.localEulerAngles += new Vector3(90, 0, 0);
+        _credits.transform.localPosition = new Vector3(0.01f, -0.011f, 0.002f);
     }
 
     //void SetTextToScoreboard(TextMesh text, int fontsize, 
@@ -146,10 +160,21 @@ public class Scoreboard : MonoBehaviour, IOnGameLossTarget
         //Lifes update
         _remaininglives = GameObject.Find("Player").GetComponent<PlayerStats>().Lives;
         _lives.text = "Lives: " + _remaininglives;
+
+        //Credit update
+        _playercredits = GameObject.Find("Player").GetComponent<PlayerStats>().Credits;
+        _credits.text = "Credits: " + _playercredits;
     }
 
     public void OnGameLoss()
     {
+        _remaininglives = GameObject.Find("Player").GetComponent<PlayerStats>().Lives;
+        _lives.text = "Lives: " + _remaininglives;
         enabled = false;
+    }
+
+    public void PointGain(float points)
+    {
+        Score += points;
     }
 }
