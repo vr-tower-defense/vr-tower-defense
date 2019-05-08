964c8e26b490b1ad185ce2ec4f074d3879aa3bf4
Lex van der Stap <lex_olifant@hotmail.com>
2019-05-08 14:57:46 +0200

    #85 Put .Blend/fbx files in Assets folder, Scoreboard implementation complete,

--- src/Assets/LightSkyDefense/Scripts/Scoreboard.cs
+++ src/Assets/LightSkyDefense/Scripts/Scoreboard.cs
@@ -12,6 +12,7 @@ public class Scoreboard : MonoBehaviour, IOnGameLossTarget
     private float _remaininglives;
     private float _playercredits;
     private float _timeToNextWave;
+
     private AudioSource _source;
     private TextMesh _score;
     private TextMesh _timer;
@@ -20,157 +21,178 @@ public class Scoreboard : MonoBehaviour, IOnGameLossTarget
     private TextMesh _waveProgression;
     private TextMesh _credits;
 
+    private TextMesh _scoreClone;
+    private TextMesh _timerClone;
+    private TextMesh _timerToNextWaveClone;
+    private TextMesh _livesClone;
+    private TextMesh _waveProgressionClone;
+    private TextMesh _creditsClone;
+
 
     // Start is called before the first frame update
     void Start()
     {
         _source = GetComponent<AudioSource>();
         _timeToNextWave = TimeBetweenWaves;
-        //SetTextToScoreboard(_score, 30, 0.0005f, 0.0005f, 0.0005f, TextAnchor.MiddleLeft, -0.005f, -0.011f, -0.005f);
 
-
-        GameObject scoreText = new GameObject();
-        _score = scoreText.AddComponent<TextMesh>();
+        //Scoreboard Front
+        //Score
+        GameObject scoreText = gameObject.transform.Find("ScoreText").gameObject;
+        _score = scoreText.GetComponent<TextMesh>();
         _score.fontSize = 30;
-        _score.transform.SetParent(this.transform);
         _score.transform.localScale = new Vector3(0.0005f, 0.0005f, 0.0005f);
         _score.anchor = TextAnchor.MiddleLeft;
-        _score.transform.localPosition = new Vector3(-0.01f, -0.011f, -0.002f);
-        //_score.transform.localEulerAngles += new Vector3(0, 0, 0);
-        //_score.font = Resources.Load<Font>("Fonts/Potra");
-        Instantiate(scoreText, new Vector3(-0.01f, -0.011f, -0.002f), Quaternion.Euler(0, 0, 180), this.transform);
+        _score.transform.localEulerAngles = new Vector3(-90, 0, 0);
+        _score.transform.localPosition = new Vector3(-0.01f, -0.0115f, -0.002f);
 
-
-        GameObject timerText = new GameObject();
-        _timer = timerText.AddComponent<TextMesh>();
+        //Time
+        GameObject timerText = gameObject.transform.Find("TimerText").gameObject;
+        _timer = timerText.GetComponent<TextMesh>();
         _timer.fontSize = 30;
-        //_score.font = Resources.Load<Font>("Fonts/Potra");
-        _timer.transform.SetParent(this.transform);
         _timer.transform.localScale = new Vector3(0.0005f, 0.0005f, 0.0005f);
         _timer.anchor = TextAnchor.MiddleLeft;
-        //_timer.transform.localEulerAngles += new Vector3(90, 0, 0);
-        _timer.transform.localPosition = new Vector3(-0.01f, -0.011f, 0.002f);
+        _timer.transform.localEulerAngles = new Vector3(-90, 0, 0);
+        _timer.transform.localPosition = new Vector3(-0.01f, -0.0115f, 0.002f);
 
-        GameObject timerToNextWaveText = new GameObject();
-        _timerToNextWave = timerToNextWaveText.AddComponent<TextMesh>();
+        //Time to next wave
+        GameObject timerToNextWaveText = gameObject.transform.Find("TimeToNextWaveText").gameObject;
+        _timerToNextWave = timerToNextWaveText.GetComponent<TextMesh>();
         _timerToNextWave.fontSize = 30;
-        //_score.font = Resources.Load<Font>("Fonts/Potra");
-        _timerToNextWave.transform.SetParent(this.transform);
         _timerToNextWave.transform.localScale = new Vector3(0.0005f, 0.0005f, 0.0005f);
         _timerToNextWave.anchor = TextAnchor.MiddleLeft;
-        //_timer.transform.localEulerAngles += new Vector3(90, 0, 0);
-        _timerToNextWave.transform.localPosition = new Vector3(-0.008f, -0.011f, 0.005f);
+        _timerToNextWave.transform.localEulerAngles = new Vector3(-90, 0, 0);
+        _timerToNextWave.transform.localPosition = new Vector3(-0.008f, -0.0115f, 0.005f);
 
-        GameObject waveProgressionText = new GameObject();
-        _waveProgression = waveProgressionText.AddComponent<TextMesh>();
+        //Wave Progression
+        GameObject waveProgressionText = gameObject.transform.Find("WaveProgressionText").gameObject;
+        _waveProgression = waveProgressionText.GetComponent<TextMesh>();
         _waveProgression.fontSize = 30;
-        //_score.font = Resources.Load<Font>("Fonts/Potra");
-        _waveProgression.transform.SetParent(this.transform);
         _waveProgression.transform.localScale = new Vector3(0.0005f, 0.0005f, 0.0005f);
         _waveProgression.anchor = TextAnchor.MiddleLeft;
-        //_timer.transform.localEulerAngles += new Vector3(90, 0, 0);
-        _waveProgression.transform.localPosition = new Vector3(-0.004f, -0.011f, -0.005f);
+        _waveProgression.transform.localEulerAngles = new Vector3(-90, 0, 0);
+        _waveProgression.transform.localPosition = new Vector3(-0.004f, -0.0115f, -0.005f);
 
-        GameObject remainingLivesText = new GameObject();
-        _lives = remainingLivesText.AddComponent<TextMesh>();
+        //Remaining lives
+        GameObject remainingLivesText = gameObject.transform.Find("LivesText").gameObject;
+        _lives = remainingLivesText.GetComponent<TextMesh>();
         _lives.fontSize = 30;
-        //_score.font = Resources.Load<Font>("Fonts/Potra");
-        _lives.transform.SetParent(this.transform);
         _lives.transform.localScale = new Vector3(0.0005f, 0.0005f, 0.0005f);
         _lives.anchor = TextAnchor.MiddleRight;
-        //_timer.transform.localEulerAngles += new Vector3(90, 0, 0);
-        _lives.transform.localPosition = new Vector3(0.01f, -0.011f, -0.002f);
+        _lives.transform.localEulerAngles = new Vector3(-90, 0, 0);
+        _lives.transform.localPosition = new Vector3(0.01f, -0.0115f, -0.002f);
 
-        GameObject creditsText = new GameObject();
-        _credits = creditsText.AddComponent<TextMesh>();
+        //Credits
+        GameObject creditsText = gameObject.transform.Find("CreditText").gameObject;
+        _credits = creditsText.GetComponent<TextMesh>();
         _credits.fontSize = 30;
-        //_score.font = Resources.Load<Font>("Fonts/Potra");
-        _credits.transform.SetParent(this.transform);
         _credits.transform.localScale = new Vector3(0.0005f, 0.0005f, 0.0005f);
         _credits.anchor = TextAnchor.MiddleRight;
-        //_timer.transform.localEulerAngles += new Vector3(90, 0, 0);
-        _credits.transform.localPosition = new Vector3(0.01f, -0.011f, 0.002f);
-    }
+        _credits.transform.localEulerAngles = new Vector3(-90, 0, 0);
+        _credits.transform.localPosition = new Vector3(0.01f, -0.0115f, 0.002f);
 
-    //void SetTextToScoreboard(TextMesh text, int fontsize, 
-    //    float scaleX, float scaleY, float scaleZ, TextAnchor anchor,
-    //    float positionX, float positionY, float positionZ)
-    //{
-    //    GameObject textObject = new GameObject();
-    //    TextMesh _text = text;
-    //    _text = textObject.AddComponent<TextMesh>();
-    //    _text.fontSize = fontsize;
-    //    _text.transform.SetParent(this.transform);
-    //    _text.transform.localScale = new Vector3(scaleX, scaleY, scaleZ);
-    //    _text.anchor = anchor;
-    //    _text.transform.localPosition = new Vector3(positionX, positionY, positionZ);
-    //}
+        //Scoreboard back
+        //Score
+        _scoreClone = Instantiate(_score, transform);
+        _scoreClone.transform.localScale = new Vector3(-0.0005f, 0.0005f, 0.0005f);
+        _scoreClone.transform.localPosition = new Vector3(0.01f, 0.0115f, -0.002f);
+
+        //Time
+        _timerClone = Instantiate(_timer, transform);
+        _timerClone.transform.localScale = new Vector3(-0.0005f, 0.0005f, 0.0005f);
+        _timerClone.transform.localPosition = new Vector3(0.01f, 0.0115f, 0.002f);
+
+        //Time to next wave
+        _timerToNextWaveClone = Instantiate(_timerToNextWave, transform);
+        _timerToNextWaveClone.transform.localScale = new Vector3(-0.0005f, 0.0005f, 0.0005f);
+        _timerToNextWaveClone.transform.localPosition = new Vector3(0.008f, 0.0115f, 0.005f);
+
+        //Wave progression
+        _waveProgressionClone = Instantiate(_waveProgression, transform);
+        _waveProgressionClone.transform.localScale = new Vector3(-0.0005f, 0.0005f, 0.0005f);
+        _waveProgressionClone.transform.localPosition = new Vector3(0.004f, 0.0115f, -0.005f);
+
+        //Remaining lives
+        _livesClone = Instantiate(_lives, transform);
+        _livesClone.transform.localScale = new Vector3(-0.0005f, 0.0005f, 0.0005f);
+        _livesClone.transform.localPosition = new Vector3(-0.01f, 0.0115f, -0.002f);
+
+        //Credits
+        _creditsClone = Instantiate(_credits, transform);
+        _creditsClone.transform.localScale = new Vector3(-0.0005f, 0.0005f, 0.0005f);
+        _creditsClone.transform.localPosition = new Vector3(-0.01f, 0.0115f, 0.002f);
+    }
 
     // Update is called once per frame
     void Update()
     {
         //Score update
-        _score.text = $"Score: ";
+        _score.text = $"Score: {Score}";
+        _scoreClone.text = _score.text;
 
         //Time update
         Timer += Time.deltaTime;
         string minutes = Mathf.Floor(Timer / 60).ToString("00");
         string seconds = Mathf.Floor(Timer % 60).ToString("00");
         _timer.text = $"Time: {minutes}:{seconds}";
+        _timerClone.text = _timer.text;
 
         //Time until next wave update
         if (WaveNumber != TotalNumberOfWaves)
         {
             _timeToNextWave -= Time.deltaTime;
-        }
 
-        if (_timeToNextWave <= 0.0f && WaveNumber != TotalNumberOfWaves)
-        {
-            WaveNumber++;
-            if (WaveNumber % 5 == 0)
+            if (_timeToNextWave <= 0.0f)
             {
-                _source?.PlayOneShot(BossSpawnedClip);
-            }
-            else
-            {
-                _source.Play();
-            }
+                WaveNumber++;
+                if (WaveNumber % 5 == 0)
+                {
+                    _source?.PlayOneShot(BossSpawnedClip);
+                }
+                else
+                {
+                    _source.Play();
+                }
 
-            if (WaveNumber == TotalNumberOfWaves)
-            {
-                _timerToNextWave.text = "No new waves.";
-                return;
+                if (WaveNumber == TotalNumberOfWaves)
+                {
+                    _timerToNextWave.text = "No new waves.";
+                    _timerToNextWaveClone.text = _timerToNextWave.text;
+                    return;
+                }
+
+                _timeToNextWave = TimeBetweenWaves;
             }
-
-            _timeToNextWave = TimeBetweenWaves;
-        }
-
-        if (WaveNumber != TotalNumberOfWaves)
-        {
             string minutesToNextWave = Mathf.Floor(_timeToNextWave / 60).ToString("00");
             string secondsToNextWave = Mathf.Ceil(_timeToNextWave % 60).ToString("00");
             _timerToNextWave.text = $"Time to next wave: {minutesToNextWave}:{secondsToNextWave}";
+            _timerToNextWaveClone.text = _timerToNextWave.text;
         }
 
         //Wave progression update
-        _waveProgression.text = $"Wave: {WaveNumber} / {TotalNumberOfWaves}"; ;
+        _waveProgression.text = $"Wave: {WaveNumber} / {TotalNumberOfWaves}";
+        _waveProgressionClone.text = _waveProgression.text;
 
         //Lifes update
         _remaininglives = GameObject.Find("Player").GetComponent<PlayerStats>().Lives;
         _lives.text = $"Lives: {_remaininglives}";
+        _livesClone.text = _lives.text;
 
         //Credit update
         _playercredits = GameObject.Find("Player").GetComponent<PlayerStats>().Credits;
         _credits.text = $"Credits: {_playercredits}";
+        _creditsClone.text = _credits.text;
     }
 
+    //When the player loses
     public void OnGameLoss()
     {
         _remaininglives = GameObject.Find("Player").GetComponent<PlayerStats>().Lives;
         _lives.text = $"Lives: {_remaininglives}";
+        _livesClone.text = _lives.text;
         enabled = false;
     }
 
+    //Points gained when enemy dies
     public void PointGain(float points)
     {
         Score += points;
