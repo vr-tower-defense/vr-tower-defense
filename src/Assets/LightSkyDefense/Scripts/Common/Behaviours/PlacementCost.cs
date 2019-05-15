f9a3468cac86137496eaa576a91d2ce2c173a426
Leroy Korterink <s1106266@student.windesheim.nl>
2019-05-15 15:19:12 +0200

    Create PlacementCost script #119

---
+++ src/Assets/LightSkyDefense/Scripts/Common/Behaviours/PlacementCost.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+class PlacementCost : MonoBehaviour
+{
+    [Min(0)]
+    public float Cost;
+
+    private void Start()
+    {
+        var playerStatistics = Player.instance.GetComponent<PlayerStatistics>();
+
+        var outOfFunds = !playerStatistics.UpdateFunds(-Cost);
+
+        if (outOfFunds)
+        {
+            // Funds are not updated, this means that the player doesn't have enough credits.
+            Destroy(gameObject);
+        }
+    }
+}
