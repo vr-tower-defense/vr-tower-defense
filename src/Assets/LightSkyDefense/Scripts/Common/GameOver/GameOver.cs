ad432afb8842d2048b70d7e4e33e455db54c6bcb
hassanAlAwwadi <hassan.awwadi@gmail.com>
2019-05-16 14:53:18 +0200

    Fix by doing the pause game trick #139

---
+++ src/Assets/LightSkyDefense/Scripts/Common/GameOver/GameOver.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class GameOver : MonoBehaviour, IOnGameLossTarget
+{
+    public GameObject GameOverPrefab;
+    public bool Lost = false;
+
+
+    public void OnGameLoss()
+    {
+        if (Lost)
+            return;
+        Lost = true;
+        var camera = Camera.main;
+
+        if (camera == null)
+            return;
+
+        var a = Instantiate(
+            GameOverPrefab,
+            Player.instance.headCollider.transform.position + (Player.instance.headCollider.transform.rotation * new Vector3(0, 0, 1.5f)),
+            Player.instance.headCollider.transform.rotation
+        );
+        a.name = "game over screen";
+
+
+
+        var greyScale = camera.gameObject.GetComponent<GreyscaleAfterEffect>();
+
+        if (greyScale == null)
+            return;
+
+        greyScale.Active = true;
+    }
+}
