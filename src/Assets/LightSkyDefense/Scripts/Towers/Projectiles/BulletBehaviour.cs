c90450f22b6cc641454883318c473ac40c12734a
Leroy Korterink <s1106266@student.windesheim.nl>
2019-05-20 18:46:34 +0200

    Fix damageable on enemy #119

--- src/Assets/LightSkyDefense/Scripts/Towers/Projectiles/BulletBehaviour.cs
+++ src/Assets/LightSkyDefense/Scripts/Towers/Projectiles/BulletBehaviour.cs
@@ -3,31 +3,22 @@
 public class BulletBehaviour : MonoBehaviour
 {
     [Tooltip("The amount of damage that is applied to a target that collides with this gameObject")]
-    public float BulletDamage;
+    public float BulletDamage = 1;
 
     [Tooltip("The time before a bullet is removed from the scene")]
     public float TimeAlive = 3;
 
-    // The bullets own rigidbody
-    public Rigidbody _rigidbody;
-
     // Start is called before the first frame update
     void Start()
     {
-        _rigidbody = GetComponent<Rigidbody>();
-
         Destroy(gameObject, TimeAlive);
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        var rigidbody = collision
-            .gameObject
-            .GetComponent<Rigidbody>();
-
         var damagable = collision
             .gameObject
-            .GetComponent<Damagable>();
+            .GetComponent<Damageable>();
 
         // Decrease target health
         damagable?.UpdateHealth(-BulletDamage);
