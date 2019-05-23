22496cf2ebd5ed656e40b9318e671f97a8594e0a
mserier <seriermax@gmail.com>
2019-05-23 12:50:59 +0200

    Store greymaterial and reuse it #82
    Changed tower to sphere collider instead of boxcollider
    
    Fix MissleLauncher scale (adjusted import settings)

--- src/Assets/LightSkyDefense/Scripts/Common/Behaviours/WorldPlaceable.cs
+++ src/Assets/LightSkyDefense/Scripts/Common/Behaviours/WorldPlaceable.cs
@@ -2,34 +2,65 @@
 
 public class WorldPlaceable : MonoBehaviour
 {
-    public BoxCollider ObjectCollider;
+    public SphereCollider ObjectCollider;
 
     [Header("True makes the object InvalidColor when it's colliding, false will remove the object when it's not allowed")]
     public bool isVisual = true;
 
     public Color InvalidColor = new Color(0.2f, 0.2f, 0.2f);
 
+    public bool isPlaceValid { get; private set; }
+
     /// <summary>
-    /// Keep track of the original material so we can revert to it
+    /// Keep track of the original material so we can revert to it.
     /// </summary>
     private Material _originalPrefabMat;
+
+    /// <summary>
+    /// Store the gray material so we don't have to recreate it every time we need it.
+    /// </summary>
+    private Material _grayMaterial;
 
     private static Layers _layerMask = Layers.Path|Layers.Enemies|Layers.Towers;
 
     void Awake()
     {
-        if (!isVisual)
+        if (isVisual)
+            return;
+
+        foreach (Collider collider in Physics.OverlapSphere(ObjectCollider.transform.position, ObjectCollider.radius, (int)_layerMask))
         {
-            foreach (Collider collider in Physics.OverlapBox(ObjectCollider.transform.position, ObjectCollider.size / 2f, transform.rotation, (int)_layerMask))
+            if(collider.transform != ObjectCollider.transform)
             {
-                if(collider.transform != ObjectCollider.transform)
-                {
-                    Destroy(gameObject);
-                    return;
-                }
+                Destroy(gameObject);
+                return;
             }
-            Destroy(this);
         }
+        Destroy(this);
+    }
+
+    void Start()
+    {
+        // Store original material
+        _originalPrefabMat = GetComponentInChildren<Renderer>().material;
+
+        // Clone original material
+        _grayMaterial = new Material(_originalPrefabMat);
+
+        // Set color to gray
+        _grayMaterial.color = InvalidColor;
+
+        //Do the check once to initialize isPlaceValid correctly
+
+        if (Physics.CheckSphere(ObjectCollider.transform.position, ObjectCollider.radius, (int)_layerMask))
+        {
+            GetComponentInChildren<Renderer>().material = _grayMaterial;
+            isPlaceValid = false;
+            return;
+        }
+
+        GetComponentInChildren<Renderer>().material = _originalPrefabMat;
+        isPlaceValid = true;
     }
 
     void FixedUpdate()
@@ -37,28 +68,20 @@ public class WorldPlaceable : MonoBehaviour
         if (ObjectCollider == null)
             return;
 
-        if (Physics.CheckBox(ObjectCollider.transform.position, ObjectCollider.size / 2f, transform.rotation, (int)_layerMask))
+        if (Physics.CheckSphere(ObjectCollider.transform.position, ObjectCollider.radius, (int)_layerMask))
         {
-            if (_originalPrefabMat == null)
+            if(isPlaceValid)
             {
-                // Store original material
-                _originalPrefabMat = GetComponentInChildren<Renderer>().material;
-                Material grayMaterial = new Material(_originalPrefabMat);
-
-                // Set color to gray
-                grayMaterial.color = InvalidColor;
-                GetComponentInChildren<Renderer>().material = grayMaterial;
+                GetComponentInChildren<Renderer>().material = _grayMaterial;
             }
+            isPlaceValid = false;
+            return;
         }
-        else
+
+        if(!isPlaceValid)
         {
-            if (_originalPrefabMat != null)
-            {
-                // Restore original material
-                GetComponentInChildren<Renderer>().material = _originalPrefabMat;
-
-                _originalPrefabMat = null;
-            }
+            GetComponentInChildren<Renderer>().material = _originalPrefabMat;
         }
+        isPlaceValid = true;
     }
 }
