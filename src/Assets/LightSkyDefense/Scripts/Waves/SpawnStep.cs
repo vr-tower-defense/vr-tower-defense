b50d55c6f6bae1fa5057f13db4ff7a0a4969d403
Leroy Korterink <s1106266@student.windesheim.nl>
2019-05-28 10:53:28 +0200

    Remove unused using statement #148

--- src/Assets/LightSkyDefense/Scripts/Waves/SpawnStep.cs
+++ src/Assets/LightSkyDefense/Scripts/Waves/SpawnStep.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "SpawnStep", menuName = "Waves/SpawnStep", order = 1)]
 public class SpawnStep : WaveStep
 {
     public GameObject Enemy;
+
     public override IEnumerator Run()
     {
         yield return Instantiate(
