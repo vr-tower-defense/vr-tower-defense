024575cd6cbec1d37523fb9e73163be0417c9058
Max <max@ilmer.nl>
2019-05-29 10:31:52 +0200

    Add creditsDisplay as subscriber to playerStats #170

--- src/Assets/LightSkyDefense/Scripts/Controls/Dial/CurrentCreditsDisplay.cs
+++ src/Assets/LightSkyDefense/Scripts/Controls/Dial/CurrentCreditsDisplay.cs
@@ -6,22 +6,25 @@ using Valve.VR.InteractionSystem;
 public class CurrentCreditsDisplay : MonoBehaviour
 {
     [Tooltip("Credit text prefab")]
-    public GameObject CurrentCreditText;
+    public GameObject CreditTextPrefab;
 
-    private GameObject _textMesh;
+    private GameObject _creditsText;
 
     void Start()
     {
-        var costMesh = CurrentCreditText.GetComponent<TextMesh>();
-        costMesh.text = "Currency:";
-
-        _textMesh = Instantiate(CurrentCreditText,
+        _creditsText = Instantiate(CreditTextPrefab,
             gameObject.transform.position + new Vector3(0, 0, -0.2f), // Small offset towards wrist
             gameObject.transform.rotation);
     }
 
     void Update()
     {
-        _textMesh.transform.rotation = Player.instance.headCollider.transform.rotation;
+        _creditsText.transform.rotation = Player.instance.headCollider.transform.rotation;
+    }
+
+    void OnPlayerStatisticsUpdate(PlayerStatistics playerStatistics)
+    {
+        var textMesh = _creditsText.GetComponent<TextMesh>();
+        textMesh.text = "Currency:\n" + playerStatistics.Funds;
     }
 }
