ad8181f934d64d78b29525637d6459ba006cd5eb
Lex van der Stap <lex_olifant@hotmail.com>
2019-06-03 15:46:29 +0200

    Towers have a simple health bar #190

---
+++ src/Assets/LightSkyDefense/Scripts/Towers/HealthBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider HealthBarElement;
+    public GameObject Tower;
+
+    private Damageable _damageable;
+
+    void Start()
+    {
+        _damageable = Tower.GetComponent<Damageable>();
+
+        HealthBarElement.maxValue = _damageable.MaxHealth;
+    }
+
+    void OnUpdateHealth()
+    {
+        HealthBarElement.value = _damageable.Health;
+    }
+}
