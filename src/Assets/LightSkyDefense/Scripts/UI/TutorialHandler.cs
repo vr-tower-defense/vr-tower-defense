475ca39ffadd33238b0b87f245c2c281b0c01999
Leroy Korterink <s1106266@student.windesheim.nl>
2019-06-05 22:00:55 +0200

    Add controller actions for tutorial #171

--- src/Assets/LightSkyDefense/Scripts/UI/TutorialHandler.cs
+++ src/Assets/LightSkyDefense/Scripts/UI/TutorialHandler.cs
@@ -5,98 +5,84 @@ using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityEngine.Video;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
 
 public class TutorialHandler : MonoBehaviour
 {
     public Transform VideoPlayerParent;
-    public Canvas ButtonCanvas;
-    public Button NextButton;
+
+    [Header("Button hints")]
+    public SteamVR_Action_Boolean PlayTutorialAction = SteamVR_Input.GetBooleanAction("PlayTutorial");
+    public SteamVR_Input_Sources Hand = SteamVR_Input_Sources.LeftHand;
+    public string PlayTutorialActionHint = "Start tutorial";
+    public string SkipTutorialActionHint = "Skip";
 
     private VideoPlayer _videoPlayer;
+    private readonly Queue<IEnumerator> _videoActions = new Queue<IEnumerator>(4);
 
-    private int _tutorialIndex;
-
-    private Queue<IEnumerator> _videoActions = new Queue<IEnumerator>(4);
+    private IEnumerator _tutorialEnumerator;
 
     void Start()
     {
+        _tutorialEnumerator = CreateTutorial();
+        _tutorialEnumerator.MoveNext();
+
         _videoPlayer = VideoPlayerParent.GetComponentInChildren<VideoPlayer>(true);
         _videoPlayer?.Prepare();
+
         StartCoroutine(ProcessQueue(_videoActions));
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+        if (!PlayTutorialAction.GetStateDown(SteamVR_Input_Sources.Any))
         {
-            OnTutorialClick();
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            OnStartClick();
-        }
-    }
-
-    public void OnTutorialClick()
-    {
-        if (_videoPlayer == null)
             return;
-
-        ButtonCanvas?.gameObject.SetActive(false);
-
-        switch (_tutorialIndex)
-        {
-            case 0:
-                //Show "You" slide
-                _tutorialIndex++;
-                VideoPlayerParent.gameObject.SetActive(true);
-                NextButton.gameObject.SetActive(false);
-                _videoActions.Enqueue(PlayForSec(_videoPlayer, 3.5f, () => NextButton.gameObject.SetActive(true)));
-                break;
-            case 1:
-                //Show "Enemy" slide
-                _tutorialIndex++;
-                NextButton.gameObject.SetActive(false);
-                _videoActions.Enqueue(PlayForSec(_videoPlayer, 3.8f, () => NextButton.gameObject.SetActive(true)));
-                break;
-            case 2:
-                //Show "Path" slide
-                _tutorialIndex++;
-                NextButton.gameObject.SetActive(false);
-                _videoActions.Enqueue(PlayForSec(_videoPlayer, 4.5f, () => NextButton.gameObject.SetActive(true)));
-                break;
-            case 3:
-                //Show "Path Game Preview" slide
-                _tutorialIndex++;
-                NextButton.gameObject.SetActive(false);
-                _videoActions.Enqueue(SeekSec(_videoPlayer, 15.0f));
-                _videoActions.Enqueue(PlayForSec(_videoPlayer, 3.5f, () => NextButton.gameObject.SetActive(true)));
-                break;
-            case 4:
-                //Show "Tower Placement" slide
-                _tutorialIndex++;
-                NextButton.gameObject.SetActive(false);
-                _videoActions.Enqueue(SeekSec(_videoPlayer, 33.0f));
-                _videoActions.Enqueue(PlayForSec(_videoPlayer, 26.8f, () => NextButton.gameObject.SetActive(true)));
-                break;
-            default:
-                //End presetation, show lobby menu again
-                _tutorialIndex = 0;
-                NextButton.gameObject.SetActive(false);
-                VideoPlayerParent.gameObject.SetActive(false);
-                ButtonCanvas?.gameObject.SetActive(true);
-                break;
         }
+
+        _tutorialEnumerator.MoveNext();
     }
 
-    public void OnStartClick()
+    public IEnumerator CreateTutorial()
     {
-        SceneManager.LoadScene("LSDScene");
+        var hand = Player.instance.GetHand(Hand);
+
+        // Show start hints
+        ControllerButtonHints.HideTextHint(hand, PlayTutorialAction);
+        ControllerButtonHints.ShowTextHint(hand, PlayTutorialAction, PlayTutorialActionHint);
+
+        yield return null;
+
+        // Show skip hints
+        ControllerButtonHints.HideTextHint(hand, PlayTutorialAction);
+        ControllerButtonHints.ShowTextHint(hand, PlayTutorialAction, SkipTutorialActionHint);
+
+        //Show "You" slide
+        _videoActions.Enqueue(PlayForSec(_videoPlayer, 3.5f));
+        yield return null;
+
+        //Show "Enemy" slide
+        _videoActions.Enqueue(PlayForSec(_videoPlayer, 3.8f));
+        yield return null;
+
+        //Show "Path" slide
+        _videoActions.Enqueue(PlayForSec(_videoPlayer, 4.5f));
+        yield return null;
+
+        //Show "Path Game Preview" slide
+        _videoActions.Enqueue(SeekSec(_videoPlayer, 15.0f));
+        _videoActions.Enqueue(PlayForSec(_videoPlayer, 3.5f));
+        yield return null;
+
+        _videoActions.Enqueue(SeekSec(_videoPlayer, 33.0f));
+        _videoActions.Enqueue(PlayForSec(_videoPlayer, 26.8f));
+        yield return null;
     }
 
     public IEnumerator ProcessQueue(Queue<IEnumerator> queue)
     {
-        while(true)
+        while (true)
         {
             if (queue.Count > 0)
             {
--- src/Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ src/Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -113,6 +113,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Boolean PlayTutorial
+        {
+            get
+            {
+                return SteamVR_Actions.default_PlayTutorial;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
