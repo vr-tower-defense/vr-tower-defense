35a132c4d00ec071a6a64c84ada03ae55e4a6aa7
Max <max@ilmer.nl>
2019-06-05 23:15:02 +0200

    Replace OnDie broadcast to EndGoal #215

---
+++ src/Assets/LightSkyDefense/Scripts/Path/EndGoal.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class EndGoal : MonoBehaviour
+{
+    [Tooltip("Speed at which the Goal rotates")]
+    public float RotationSpeed = 10f;
+
+    private PlayerStatistics _playerStatistics;
+
+    private void Start()
+    {
+        _playerStatistics = Player.instance.GetComponent<PlayerStatistics>();
+    }
+
+    void FixedUpdate()
+    {
+        transform.Rotate(Vector3.up, RotationSpeed * Time.deltaTime, Space.World);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        var enemy = collision.gameObject.GetComponent<Enemy>();
+
+        // Reduce player lives
+        _playerStatistics.UpdateLives(-enemy.Damage);
+
+        // Destroy enemy
+        enemy.BroadcastMessage(
+            "OnDie",
+            null,
+            SendMessageOptions.DontRequireReceiver
+        );
+    }
+}
