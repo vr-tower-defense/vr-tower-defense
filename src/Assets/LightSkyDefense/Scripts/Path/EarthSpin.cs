63bc2ee73712c38445f77d029fa3bc5dd360cdb2
Max <max@ilmer.nl>
2019-06-05 15:28:30 +0200

    Renamed files to valid convention #215

---
+++ src/Assets/LightSkyDefense/Scripts/Path/EarthSpin.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EarthSpin : MonoBehaviour
+{
+    [Tooltip("Speed at which the Earth rotates")]
+    public float RotationSpeed = 10f;
+
+    void FixedUpdate()
+    {
+        transform.Rotate(Vector3.up, RotationSpeed * Time.deltaTime, Space.World);
+    }
+}
